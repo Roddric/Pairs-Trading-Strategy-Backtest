0f0dd4389de6f527b1ef845d3a50aa0664eae526
Roddric <limbertroddric@gmail.com>
2026-03-16 10:11:49 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import PairsWatchlist from './PairsWatchlist.jsx'
+
+const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    {path === '/watchlist' ? <PairsWatchlist /> : <App />}
+  </React.StrictMode>
 )
