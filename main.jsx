3565b16da6539aab7b7567a5862a7efb8cce23dd
Roddric <limbertroddric@gmail.com>
2026-03-16 10:37:59 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,19 +1,18 @@
 import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-const PairsWatchlist = lazy(() => import('./Pairswatchlist.jsx'))
-const TradingScanner = lazy(() => import('./TradingScanner.jsx'))
+import TradingScanner from './TradingScanner.jsx'
 
 const path = window.location.pathname
 const Loading = () => <div style={{background:'#05070e',minHeight:'100vh'}}/>
 
+let Page = App
+if (path === '/scanner') Page = TradingScanner
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Suspense fallback={<Loading/>}>
-      {path === '/watchlist' ? <PairsWatchlist /> :
-       path === '/scanner'  ? <TradingScanner /> :
-       <App />}
+      <Page />
     </Suspense>
   </React.StrictMode>
 )
